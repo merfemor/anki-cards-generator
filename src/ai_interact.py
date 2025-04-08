c50ab1176020bf2b332ddeacc0cc5fbbd075a105
Roman Efremov <efremov.roman98@gmail.com>
2025-04-08 20:03:00 +0200

    More understandable errors in ollama requests

--- src/ai_interact.py
+++ src/ai_interact.py
@@ -6,7 +6,11 @@ import ollama
 def ask_ai(prompt: str) -> str:
     # TODO: how to set a random seed?
     logging.info(f"AI request={prompt}")
-    response = ollama.generate(model='cas/discolm-mfto-german:latest', prompt=prompt, options={"top_k": 20})
+    try:
+        response = ollama.generate(model='cas/discolm-mfto-german:latest', prompt=prompt, options={"top_k": 20})
+    except Exception as e:
+        logging.error("Exception during ollama request. Did you forgot to start the service?", exc_info=True)
+        raise Exception("Exception during ollama request") from e
     response_text = response['response']
     logging.info(f"AI response={response_text}")
     return response_text
