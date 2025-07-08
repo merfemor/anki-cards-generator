445dcb3c806eb35a470d1479293411e5d69c3ce1
Roman Efremov <efremov.roman98@gmail.com>
2025-07-08 21:12:50 +0200

    Tests: add tests for bad requests to api endpoint

---
+++ tests/api_tests.py
@@ -0,0 +1,33 @@
+import json
+import unittest
+
+from server import app
+
+
+class ApiEndpointGenerateCardsFileTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def do_request(self, body: dict):
+        return self.app.post(
+            '/api/generateCardsFile',
+            data=json.dumps(body),
+            content_type='application/json'
+        )
+
+    def test_empty_words(self):
+        response = self.do_request({'words': [], 'language': 'de'})
+        self.assertEqual(response.status_code, 400)
+
+    def test_absent_words(self):
+        response = self.do_request({'language': 'de'})
+        self.assertEqual(response.status_code, 400)
+
+    def test_absent_language(self):
+        response = self.do_request({'words': ['Katze']})
+        self.assertEqual(response.status_code, 400)
+
+    def test_not_supported_language(self):
+        response = self.do_request({'words': ['katt'], 'language': 'sv'})
+        self.assertEqual(response.status_code, 400)
