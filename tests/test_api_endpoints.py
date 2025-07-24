6014c2b2b195f4583ed6513b0e87c557d05b2ac7
Roman Efremov <efremov.roman98@gmail.com>
2025-07-24 21:05:31 +0200

    Migrate API endpoint tests from unittest to pytest

--- tests/test_api_endpoints.py
+++ tests/test_api_endpoints.py
@@ -1,11 +1,10 @@
 import json
-import unittest
 
 from main import app
 
 
-class ApiEndpointsTestCase(unittest.TestCase):
-    def setUp(self):
+class TestApiEndpoints:
+    def setup_method(self):
         self.app = app.test_client()
         self.app.testing = True
 
@@ -14,20 +13,20 @@ class ApiEndpointsTestCase(unittest.TestCase):
 
     def test_empty_words(self):
         response = self.do_request({"words": [], "language": "de"})
-        self.assertEqual(response.status_code, 400)
+        assert response.status_code == 400
 
     def test_absent_words(self):
         response = self.do_request({"language": "de"})
-        self.assertEqual(response.status_code, 400)
+        assert response.status_code == 400
 
     def test_absent_language(self):
         response = self.do_request({"words": ["Katze"]})
-        self.assertEqual(response.status_code, 400)
+        assert response.status_code == 400
 
     def test_not_supported_language(self):
         response = self.do_request({"words": ["katt"], "language": "sv"})
-        self.assertEqual(response.status_code, 400)
+        assert response.status_code == 400
 
     def test_home_page_ok_response(self):
         res = self.app.get("/")
-        self.assertEqual(200, res.status_code)
+        assert res.status_code == 200
