9370e07b982f076470beaaea9edd4abac67f0288
Roman Efremov <efremov.roman98@gmail.com>
2026-05-16 12:50:06 +0200

    Extract prompt into a separate string

--- src/app/prompts.py
+++ src/app/prompts.py
@@ -2,8 +2,12 @@ from typing import Literal
 
 from app.utils import check
 
+PROMPT_TEMPLATE = """
+Generate one sentence in {LANGUAGE} using the {TYPE} "{WORD_OR_PHRASE}". The answer must only contain the sentence itself.
+""".strip()
+
 
 def get_sentence_example_prompt(word_or_phrase: str, language: Literal["German", "English"], is_phrase: bool) -> str:
     check(language in ["German", "English"], f"Unsupported language: {language}")
-    type = "phrase" if is_phrase else "word"
-    return f'Generate one sentence in {language} using the {type} "{word_or_phrase}". The answer must only contain the sentence itself.'
+    _type = "phrase" if is_phrase else "word"
+    return PROMPT_TEMPLATE.format(LANGUAGE=language, TYPE=_type, WORD_OR_PHRASE=word_or_phrase)
