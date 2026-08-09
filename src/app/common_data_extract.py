372684d50c8ad3c5278ad627496b8b592abf6280
Roman Efremov <efremov.roman98@gmail.com>
2026-08-09 17:27:43 +0200

    feat: attempt to fix too long LLM generated sentences with trash

--- src/app/common_data_extract.py
+++ src/app/common_data_extract.py
@@ -1,17 +1,19 @@
 import textwrap
-from typing import Literal
+from typing import Literal, Final
 
 from app.llm_interact import ask_llm
 from app.prompts import get_sentence_example_prompt
 from app.utils import check
 
+MAX_RESPONSE_LENGTH: Final[int] = 1000
+
 
 async def generate_sentence_example_with_llm(word: str, language: Literal["English", "German"], is_phrase: bool) -> str:
-    prompt = get_sentence_example_prompt(word, language, is_phrase)
+    prompt = get_sentence_example_prompt(word, language, is_phrase, MAX_RESPONSE_LENGTH)
 
     res = (await ask_llm(prompt)).strip()
     check(len(res) > len(word), f"Too short response: {res}")
     shortened = textwrap.shorten(res, width=120, placeholder="...")
-    check(len(res) < 1000, f"Too long response, len={len(res)}: {shortened}")
+    check(len(res) < MAX_RESPONSE_LENGTH, f"Too long response, len={len(res)}: {shortened}")
 
     return res
--- src/app/prompts.py
+++ src/app/prompts.py
@@ -3,11 +3,13 @@ from typing import Literal
 from app.utils import check
 
 PROMPT_TEMPLATE = """
-Generate one sentence in {LANGUAGE} using the {TYPE} "{WORD_OR_PHRASE}". Output only the sentence. Do not use markdown, formatting, or styling.
+Generate one sentence in {LANGUAGE} using the {TYPE} "{WORD_OR_PHRASE}". Output only the sentence. Do not use markdown, formatting, or styling. Max response length is {MAX_LENGTH} characters.
 """.strip()
 
 
-def get_sentence_example_prompt(word_or_phrase: str, language: Literal["German", "English"], is_phrase: bool) -> str:
+def get_sentence_example_prompt(
+    word_or_phrase: str, language: Literal["German", "English"], is_phrase: bool, max_length: int
+) -> str:
     check(language in ["German", "English"], f"Unsupported language: {language}")
     _type = "phrase" if is_phrase else "word"
-    return PROMPT_TEMPLATE.format(LANGUAGE=language, TYPE=_type, WORD_OR_PHRASE=word_or_phrase)
+    return PROMPT_TEMPLATE.format(LANGUAGE=language, TYPE=_type, WORD_OR_PHRASE=word_or_phrase, MAX_LENGTH=max_length)
